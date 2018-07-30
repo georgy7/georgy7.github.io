bf11328b58b2c752a425694495e61ee52a229ed6
Georgy Ustinov <httpcatharsis@gmail.com>
2018-07-31 01:25:48 +0300

    Does not work.

--- build_html.sh
+++ build_html.sh
@@ -1,2 +1,2 @@
 pandoc index.md -f markdown -t html -s -o index.html -V "pagetitle:Заметки" --toc --css simple.css
-perl -p -i -e 's/<head>/<head>\n  <meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=0.2, maximum-scale=5" \/>/g' index.html
+perl -p -i -e 's/<head>/<head>\n  <meta name="viewport" content="width=device-width, initial-scale=1" \/>/g' index.html
